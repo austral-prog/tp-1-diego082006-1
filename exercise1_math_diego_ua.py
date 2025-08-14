b5419fcaaa2432738396619812826c3f6075eac9
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-08-14 18:22:39 -0300

    Add files via upload

---
+++ exercise1_math_diego_ua.py
@@ -0,0 +1,30 @@
+def math():
+	a = 57
+	b = 7
+	print(a + b) # suma
+	resta = a - b
+	print(resta) # resta
+
+math()
+
+def math():
+	a = 113*3
+	b = 64/2
+	print(a) # multiplicación
+	print(b) # división
+
+math()
+
+def math():
+	a = 8*1
+	b = 8/8
+	print(a) # multiplicación
+	print(b) # división
+
+math()
+
+def math():
+	multiplicate = 4.071428571*2
+	print(multiplicate)
+
+math()
