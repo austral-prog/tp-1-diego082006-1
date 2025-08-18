378c8bfc512dc65951013125c770cda2819ba8ab
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-08-17 23:54:51 -0300

    Add files via upload

---
+++ ejercicio2-tp1-ua-diego.py
@@ -0,0 +1,37 @@
+i1 = 2
+i2 = 4
+i3 = i1 + i2
+print("valor de i1")
+print(i1)
+print("valor de i2")
+print(i2)
+print("valor de i3")
+print(i3)
+print(i1 + i2 + i3)
+
+s1, s2, s3 = "Python", " is ", 'awesome'
+print(s1 + s2 + s3)
+
+x = "Naranja"
+y = z = ", Naranja"
+print(x + y + z)
+
+z1 = i3 / i2
+print(z1)
+z2 = i3 % i2
+print(z2)
+f1 = -.5
+f2 = 10
+f3 = f1 + f2
+i3 = int(f3)
+print("entero de i3:")
+print(i3)
+print("variable de f3:")
+print(f3)
+f2 += i1
+print("el valor de i1 es igual a:")
+print(f2)
+print("más")
+print(f1)
+print("es:")
+print(f2 + f1)
