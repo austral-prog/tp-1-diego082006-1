96f910aa5cf732a026f965dd49c3a21b44927aed
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-08-25 12:41:33 -0300

    Add files via upload

---
+++ exercise1_tp1_math_diego.py
@@ -0,0 +1,19 @@
+def math():
+    a = 57
+    b = 7
+    suma = a + b
+    print(suma) 
+    diferencia = a - b
+    print(diferencia) 
+    producto = a*7
+    promedio = (a + b)/2
+    print(producto) 
+    print(promedio) 
+    cociente_entero = a//b
+    print(cociente_entero)
+    resto = a % b
+    print(resto)
+    división_real = a/b
+    print(división_real)
+
+math()
