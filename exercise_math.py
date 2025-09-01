fddf5d066da028c992cb4080cb69ef61d9ab2543
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-09-01 16:25:53 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,19 +1,18 @@
 def math():
     a = 57
     b = 7
-    Suma = a + b
-    print(Suma)
-    Diferencia = a - b
-    print(Diferencia)
-    Producto = a * b
-    print(Producto)
-    Promedio = (a + b) / 2
-    print(Promedio)
-    Cociente_entero = a // b
-    print(Cociente_entero)
-    Resto = a % b
-    print(Resto)
-    Division_real = a / b
-    print(Division_real)
-
+    suma = a + b
+    print(suma) # Suma
+    diferencia = a - b
+    print(diferencia) # Diferencia
+    producto = a*b
+    print(producto) # Producto
+    promedio =(a+b)/2
+    print(promedio) # Promedio
+    cociente_entero = a//b
+    print(cociente_entero) # Cociente entero
+    resto = a%b
+    print(resto) # Resto
+    división_real = a/b
+    print(división_real) # División real
 math()
