c55d4997a96d021032a623166f750afa1021f057
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-09-02 10:21:58 -0300

    Update math_test.py

--- math_test.py
+++ math_test.py
@@ -6,15 +6,15 @@ import exercise_math
 class MyTestCase(unittest.TestCase):
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_math(self, mock_stdout):
-        exercise_math.main()
+        exercise_math.math()
         results = mock_stdout.getvalue().splitlines()
         self.assertEqual(results[0], "64")  # suma
-        self.assertEqual(results[1], "50")  # diferencia
-        self.assertEqual(results[2], "399")  # producto
-        self.assertEqual(results[3], "32.0")  # promedio
-        self.assertEqual(results[4], "8")  # cociente entero
-        self.assertEqual(results[5], "1")  # resto
-        self.assertEqual(results[6], "8.142857142857142")  # division real
+        self.assertEqual(results[1], "50")  # resta
+        self.assertEqual(results[2], "399")  # add assertion here
+        self.assertEqual(results[3], "32.0")  # add assertion here
+        self.assertEqual(results[4], "8")  # add assertion here
+        self.assertEqual(results[5], "1")  # add assertion here
+        self.assertEqual(results[6], "8.142857142857142")  # add assertion here
 
 
 if __name__ == '__main__':
